634add86b1eb5f9794b190b5d587223394d593b3
Danyss09 <112229585+Danyss09@users.noreply.github.com>
2024-12-09 17:50:00 -0500

    Create server.js with GraphQL

---
+++ graphql/server.js
@@ -0,0 +1,19 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello World, my name is Daniela Cáceres!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`Server running at ${url}`);
+});
